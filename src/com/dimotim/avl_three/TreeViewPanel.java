6784bb0df9503a9f56ff49558c6dfabaa2ccfd10
dimotim <datdimotim@gmail.com>
2018-02-01 14:56:06 +0300

    treeView alfa version completed

--- src/com/dimotim/avl_three/TreeViewPanel.java
+++ src/com/dimotim/avl_three/TreeViewPanel.java
@@ -4,10 +4,58 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TreeViewPanel extends JPanel {
+    private AVLTree<Integer,Void> tree=new AVLTree<>();
+    private final int sizeOfNode=30;
+    private final int verticalOffset=20;
+
+    public void setTree(AVLTree<Integer, Void> tree){
+        this.tree=tree;
+        updateTree();
+    }
+
+    public void updateTree(){
+        updateUI();
+    }
+
     @Override
     public void paint(Graphics g) {
         Rectangle bounds=g.getClipBounds();
         Point center=new Point((bounds.x+bounds.width)/2,(bounds.y+bounds.height)/2);
-        g.drawOval(center.x-25,center.y-25,50,50);
+        Point root=new Point(center.x,sizeOfNode);
+        g.translate(root.x,root.y);
+        paintTree(g,tree.root,new Point(0,0),center.x/2);
+    }
+
+    private void paintTree(Graphics g, AVLTree.Node<Integer,Void> root, Point current, int interval){
+        if(root==null)return;
+        paintNode(g,current,root.key);
+        if(root.left!=null){
+            Point leftCenter=paintLeft(g,current,interval);
+            paintTree(g,root.left,leftCenter,interval/2);
+        }
+        if(root.right!=null){
+            Point rightCenter=paintRight(g,current,interval);
+            paintTree(g,root.right,rightCenter,interval/2);
+        }
+    }
+
+    private void paintNode(Graphics g, Point center, int val){
+        Point rightUpCorner=new Point(center.x-sizeOfNode/2,center.y-sizeOfNode/2);
+        g.drawRect(rightUpCorner.x,rightUpCorner.y,sizeOfNode,sizeOfNode);
+        g.drawString(""+val,rightUpCorner.x+7,rightUpCorner.y+20);
+    }
+
+    private Point paintLeft(Graphics g, Point center, int interval){
+        Point from=new Point(center.x-5,center.y+sizeOfNode/2);
+        Point to=new Point(center.x-interval,from.y+verticalOffset);
+        g.drawLine(from.x,from.y,to.x,to.y);
+        return new Point(to.x,to.y+sizeOfNode/2);
+    }
+
+    private Point paintRight(Graphics g, Point center, int interval){
+        Point from=new Point(center.x+5,center.y+sizeOfNode/2);
+        Point to=new Point(center.x+interval,from.y+verticalOffset);
+        g.drawLine(from.x,from.y,to.x,to.y);
+        return new Point(to.x,to.y+sizeOfNode/2);
     }
 }
--- src/com/dimotim/avl_three/Viewer.java
+++ src/com/dimotim/avl_three/Viewer.java
@@ -8,6 +8,7 @@ public class Viewer extends JFrame{
     private JButton addButton;
     private JButton deleteButton;
     private TreeViewPanel treeViewPanel;
+    private AVLTree<Integer, Void> tree=new AVLTree<>();
 
     public static void main(String[] args) {
         new Viewer();
@@ -16,8 +17,21 @@ public class Viewer extends JFrame{
     public Viewer(){
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setContentPane(mainPanel);
+        treeViewPanel.setTree(tree);
+        setListeners();
         setSize(800,600);
         setVisible(true);
+    }
+
+    void setListeners(){
+        addButton.addActionListener(e -> {
+            tree.insert(Integer.parseInt(textField.getText()),null);
+            treeViewPanel.updateTree();
+        });
+        deleteButton.addActionListener(e -> {
+            tree.delete(Integer.parseInt(textField.getText()));
+            treeViewPanel.updateTree();
+        });
     }
 }
 
